dca059939f83da0e3df487df2294e920c8b54141
daniel-martin-garcia <danielmartingarcia@outlook.es>
2020-01-07 19:35:16 +0100

    First commit

---
+++ commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIRECTORY=$(pwd)
+echo "$DIRECTORY"
+tar -xvzf apache-tomcat.tar.gz
+tar -zxvf jre-8u231-linux-x64.tar.gz
+cp server.txt apache-tomcat/webapps/ROOT
+cd $DIRECTORY/apache-tomcat/bin
+export JRE_HOME=$DIRECTORY/jre1.8.0_231
+chmod +x ./startup.sh
+sh ./startup.sh 
+
+#tar -zvcf apache-tomcat.tar.gz apache-tomcat
